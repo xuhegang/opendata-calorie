66f80c692b028ed181daff3985244311486eb260
Ivarlock-liu <ivarlock.liu@gmail.com>
2018-05-10 18:29:06 +0100

    add a new page

---
+++ WebApplication13/WebApplication13/Controllers/DetailController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication13.Controllers
+{
+    public class DetailController : Controller
+    {
+        // GET: Detail
+        public ActionResult Recipe()
+        {
+            return View();
+        }
+    }
+}
--- WebApplication13/WebApplication13/js/showRecipes.js
+++ WebApplication13/WebApplication13/js/showRecipes.js
@@ -1,27 +1,27 @@
 ﻿window.onload = function () {
-    var figuretype = document.getElementsByClassName("figuretype");
-    var blanktype = document.getElementsByClassName("blanktype")
-    var recipeswrapper = document.getElementsByClassName("recipeswrapper");
+    var figuretype = document.getelementsbyclassname("figuretype");
+    var blanktype = document.getelementsbyclassname("blanktype")
+    var recipeswrapper = document.getelementsbyclassname("recipeswrapper");
     for (var i = 0; i < figuretype.length; i++) {
         figuretype[i].onclick = function (event) {
-            event.stopPropagation();
-            event.preventDefault();
+            event.stoppropagation();
+            event.preventdefault();
         }
     }
     for (var j = 0; j < blanktype.length; j++) {
         blanktype[j].onclick = function (event) {
-            event.stopPropagation();
-            event.preventDefault();
+            event.stoppropagation();
+            event.preventdefault();
             alert("blank")
         }
     }
-  //  recipeswrapper.onclick = function (event) {
-  //     console.log(event.target);
-  //  }
-    //for (var i = 0; i < recipeswrapper.length; i++) {
-    //    recipeswrapper[0].addEventListener("click", function () {
-    //        alert("hello");
-   //     });
-  //  }
+    recipeswrapper.onclick = function (event) {
+       console.log(event.target);
+    }
+    for (var i = 0; i < recipeswrapper.length; i++) {
+        recipeswrapper[0].addeventlistener("click", function () {
+            alert("hello");
+        });
+    }
     
 }
